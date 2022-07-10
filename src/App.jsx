9d35ba289d272df11bbd83d48de797d3b40c4e29
CryptoDarth <aditya.cryptd@gmail.com>
2022-07-10 20:42:18 +0530

    Base Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Spline from '@splinetool/react-spline';
+import styled from "styled-components";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <wrapper>        
+        <image src = "/home/cryptodarth/Documents/NFT_Website/src/img/Dryps_main@1-842x690.jpg"></image>
+        {/* <Spline scene="https://prod.spline.design/AkcahKn-BH9fLpFj/scene.splinecode" /> */}
+      </wrapper>
+      <h1>The Dryps</h1>
+      <h4>By Cardheads, For Carheads</h4>
     </div>
   )
 }
